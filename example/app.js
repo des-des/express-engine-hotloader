a37cb9b7f062fc34c9d5b741bf7d607453d0902a
eoin-mc-carthy <em1081@my.bristol.ac.uk>
2017-07-23 16:49:04 +0100

    adds example

---
+++ example/app.js
@@ -0,0 +1,19 @@
+const exphbs  = require('express-handlebars')
+const withHotloader = require('../')
+
+var app = require('express')()
+
+const server = require('http').Server(app)
+
+const engine = process.env.NODE_ENV !== 'production'
+  ? withHotloader(server, exphbs)
+  : exphbs
+
+app
+  .engine('handlebars', engine({ defaultLayout: 'main' }))
+  .set('view engine', 'handlebars')
+  .get('/', (req, res) => { res.render('home') })
+
+server.listen(3000, () => {
+  console.log('listening on 3000')
+})
